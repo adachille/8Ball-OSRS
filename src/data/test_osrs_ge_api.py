23d1e89f7d4db6abd21a88b063f326a278931a0e
Anthony D'Achille <adachille3@gatech.edu>
2020-02-26 18:07:33 -0500

    Check that exception is raised by get_item_details with bad id

--- src/data/test_osrs_ge_api.py
+++ src/data/test_osrs_ge_api.py
@@ -1,13 +1,21 @@
 import pytest
+import requests
 from osrs_ge_api import OldSchoolGEAPIInterface, get_all_item_ids
 
 class TestOldSchoolGEAPIInterface: 
-    def test_get_item_details(self):
+    def test_get_item_details_gets_cannonball(self):
         api_interface = OldSchoolGEAPIInterface()
         cannonball_id = 2
         cannonball_details = api_interface.get_item_details(cannonball_id)
         assert type(cannonball_details) == dict
         assert cannonball_details['name'] == "Cannonball"
+    def test_get_item_details_fails_on_bad_id(self):
+        api_interface = OldSchoolGEAPIInterface()
+        bad_id = -1
+        with pytest.raises(Exception) as excep_info:
+            api_interface.get_item_details(bad_id)
+        print(excep_info.type)
+        assert excep_info.type == requests.exceptions.HTTPError
 
 def test_get_all_item_ids():
     item_ids = get_all_item_ids()
